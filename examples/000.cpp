00001d72649852205795b750565503dfc80099fc
marko <masscry@gmail.com>
2019-08-18 02:23:06 +0300

    example: 000 uses new frame functions

--- examples/000.cpp
+++ examples/000.cpp
@@ -1,12 +1,48 @@
 #include <draw.hpp>
 #include <cstdio>
 
+class sampleFrame:public draw::frameStage_t
+{
+  void OnRender() override
+  {
+    glClear(GL_COLOR_BUFFER_BIT);
+  }
+  
+  bool IsDynamic() override
+  {
+    return false;
+  }
+};
+
 int main(int argc, char* argv[])
 {
-  draw::system_t& instance = draw::system_t::Instance();
+  try {
+    draw::system_t& instance = draw::system_t::Instance();
+    sampleFrame frame;
 
-  fprintf(stderr, "Before Sleep: %f\n", instance.Timestamp());
-  instance.Sleep(5.3);
-  fprintf(stderr, "After Sleep: %f\n", instance.Timestamp());
+    int stageID = instance.AddFrameStage(&frame);
+
+    double start = instance.Timestamp();
+    double mark  = start;
+
+    fprintf(stderr, "Start Draw: %f\n", start);
+    while (instance.IsCloseExpected() == 0)
+    {
+      double now = instance.Timestamp();
+      instance.Render();
+      if (now - mark >= 1.0)
+      {
+        fprintf(stderr, "Time Passed: %f\n", now - start);
+        mark = now;
+      }
+    }
+    fprintf(stderr, "After Draw Finished: %f\n", instance.Timestamp());
+    instance.RemoveFrameStage(stageID);
+  }
+  catch(const draw::error_t& err)
+  {
+    fprintf(stderr, "%s:%d: %s\n", err.whatFile(), err.whatLine(), err.what());
+    return -1;
+  }
   return 0;
 }
