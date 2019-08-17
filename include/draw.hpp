7f822953eca8a45da053e9a7cccae7f50071e627
marko <masscry@gmail.com>
2019-08-18 02:22:35 +0300

    draw: frameStage_t implemented, simple window

--- include/draw.hpp
+++ include/draw.hpp
@@ -15,11 +15,136 @@
 #include <draw/config.h>
 #include <cstddef>
 #include <cstdint>
+#include <cassert>
+#include <stdexcept>
+#include <vector>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 namespace draw
 {
 
+  class error_t final: public std::runtime_error {
+
+    const char* filename;
+    int line;
+
+  public:
+
+    const char* whatFile() const noexcept {
+      return this->filename;
+    }
+
+    int whatLine() const noexcept {
+      return this->line;
+    }
+
+    explicit error_t(const char* error, const char* filename, int line)
+    :std::runtime_error(error), filename(filename), line(line) {
+      ;
+    }
+
+    error_t(const error_t& copy)
+    :std::runtime_error(copy), filename(copy.filename), line(copy.line) {
+      ;
+    }
+
+    error_t& operator=(const error_t& copy) {
+      if (this != &copy)
+      {
+        this->std::runtime_error::operator=(copy);
+        this->filename = copy.filename;
+        this->line = copy.line;
+      }
+      return *this;
+    }
+
+  };
+
+#define THROW_ERROR(TEXT) throw draw::error_t(TEXT, __FILE__, __LINE__)
+
+  class frameStage_t
+  {
+    friend class system_t;
+
+    int index; /**< Index is set, when frame stage is registered in system_t */
+
+    /**
+     * @brief Called when frame stage must be rendered.
+     */
+    virtual void OnRender()  = 0;
+
+    /**
+     * @brief Must return true if this frame stage is not a singleton and
+     * allocated on heap
+     * 
+     * @return true frame stage is allocated on heap
+     * @return false frame stage is singleton
+     */
+    virtual bool IsDynamic() = 0;
+
+    frameStage_t(const frameStage_t&) = delete;
+    frameStage_t& operator = (const frameStage_t&) = delete;
+
+    frameStage_t(frameStage_t&&) = delete;
+    frameStage_t& operator = (frameStage_t&&) = delete;
+
+    /**
+     * @brief Set the ID to object
+     * 
+     * This function called in AddFrameStage/RemoveFrameStage
+     * 
+     * @param index new ID
+     */
+    void SetRegisteredID(int index)
+    {
+      this->index = index;
+    }
+
+  public:
+
+    /**
+     * @brief Get registered ID
+     * 
+     * @return int if not registered yet returns -1
+     */
+    int RegisteredID() const 
+    {
+      return this->index;
+    }
+
+    void Render() noexcept
+    {
+      this->OnRender();
+    }
+
+    void Delete() noexcept
+    {
+      if (this->IsDynamic())
+      {
+        delete this;
+      }
+    }
+
+    frameStage_t() noexcept
+    :index(-1) {
+      ;
+    }
+    
+    virtual ~frameStage_t()
+    { // Must unregister this stage before its termination 
+      assert(this->RegisteredID() < 0);
+    }
+  };
+
   class system_t final {
+
+    friend class frameStage_t;
+
+    typedef std::vector<frameStage_t*> vectorOfFrameStages_t;
+
+    GLFWwindow*           window;
+    vectorOfFrameStages_t stages;
 
     system_t(const system_t&) = delete;
     system_t& operator= (const system_t&) = delete;
@@ -33,6 +158,21 @@ namespace draw
   public:
 
     /**
+     * @brief Add new frame stage
+     * 
+     * @param stage valid frame stage pointer
+     * @return index of frame stage 
+     */
+    int AddFrameStage(frameStage_t* stage);
+
+    /**
+     * @brief Remove existing frame stage
+     * 
+     * @param id existing frame stage ID
+     */
+    void RemoveFrameStage(int id);
+
+    /**
      * @brief Get singleton instance
      * 
      * @return system_t& instance of system_t
@@ -44,14 +184,27 @@ namespace draw
      * 
      * @return double seconds with fractions passed from some point in time
      */
-    double Timestamp() const;
+    double Timestamp() const noexcept;
 
     /**
      * @brief Put current thread to sleep for N seconds with fraction
      * 
      * @param sec number of seconds to sleep with fraction
      */
-    void Sleep(double sec);
+    void Sleep(double sec) const noexcept;
+
+    /**
+     * @brief Return current window close expected status
+     * 
+     * @return true when system wants to close window
+     * @return false when system do not want to close window
+     */
+    bool IsCloseExpected() const noexcept;
+
+    /**
+     * @brief Render all registered frame stages, then swap buffers
+     */
+    void Render() noexcept;
 
   };
 
--- src/draw.cpp
+++ src/draw.cpp
@@ -8,24 +8,54 @@
  * @copyright Copyright (c) 2019
  * 
  */
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <draw.hpp>
 
 #include <ctime>
 #include <cmath>
 #include <cerrno>
 #include <cstring>
+#include <cassert>
 
 namespace draw
 {
 
-  system_t::system_t()
+  system_t::system_t():window(nullptr),stages()
   {
+    if (glfwInit() == GLFW_FALSE)
+    {
+      THROW_ERROR("Library Initialization Failed");
+    }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    this->window = glfwCreateWindow(800, 600, "draw.lib", nullptr, nullptr);
+    if (this->window == nullptr)
+    {
+      THROW_ERROR("Window Creating Failed");
+    }
+
+    glfwMakeContextCurrent(this->window);
+
+    gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+
+    glClearColor(0.16, 0.16, 0.16, 1.0);
   }
 
   system_t::~system_t()
   {
+    for(frameStage_t* stage: this->stages)
+    {
+      stage->Delete();
+    }
 
+    if (this->window != nullptr)
+    {
+      glfwDestroyWindow(this->window);
+    }
+
+    glfwTerminate();
   }
 
   system_t& system_t::Instance()
@@ -34,9 +64,54 @@ namespace draw
     return self;
   }
 
+  bool system_t::IsCloseExpected() const noexcept
+  {
+    return glfwWindowShouldClose(this->window);
+  }
+
+  void system_t::Render() noexcept
+  {
+    for(frameStage_t* stage: this->stages)
+    {
+      /*
+       * @todo implement better frame stage management 
+       */
+      if (stage != nullptr)
+      {
+        stage->Render();
+      }
+    }
+    glfwSwapBuffers(system_t::Instance().window);
+    glfwPollEvents();
+  }
+
+  int system_t::AddFrameStage(frameStage_t* stage)
+  {
+    assert(stage != nullptr);            // only valid pointers expected
+    assert(stage->RegisteredID() == -1); // only not registered stages expected
+    this->stages.push_back(stage);
+    stage->SetRegisteredID(this->stages.size()-1);
+    return stage->RegisteredID();
+  }
+
+  void system_t::RemoveFrameStage(int id)
+  {
+    assert(id < this->stages.size());
+    if (this->stages[id] != nullptr)
+    {
+      this->stages[id]->SetRegisteredID(-1);
+      this->stages[id]->Delete();
+      this->stages[id] = nullptr;
+    }
+    while (this->stages.back() == nullptr)
+    { // remove obsolete stages in very end
+      this->stages.pop_back();
+    }
+  }
+
 #if DRAW_PLATFORM_UNIX
 
-  double system_t::Timestamp() const 
+  double system_t::Timestamp() const noexcept
   {
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
@@ -44,8 +119,9 @@ namespace draw
       + static_cast<double>(ts.tv_nsec) * 1.0e-9;
   }
 
-  void system_t::Sleep(double sec)
+  void system_t::Sleep(double sec) const noexcept
   {
+    assert(sec >= 0.0);
     struct timespec ts;
     double frac = modf(sec, &sec);
     ts.tv_sec = sec;
@@ -56,7 +132,5 @@ namespace draw
   }
 
 #endif /* DRAW_PLATFORM_UNIX */
-
-
 
 } // namespace draw
