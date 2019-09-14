944e5f5c87ca589c4cf91427a78b4b6fc3512fdd
marko <masscry@gmail.com>
2019-09-15 00:33:45 +0300

    evoid: initial commit

---
+++ evoid/src/evoid.cpp
@@ -0,0 +1,25 @@
+#include <draw.hpp>
+#include <iostream>
+
+int main(int /*argc*/, char* /*argv*/[])
+{
+  try
+  {
+    auto& instance = draw::system_t::Instance();
+
+    while(instance.IsRunning())
+    {
+      instance.Render();
+      instance.Update();
+    }
+  }
+  catch(const draw::error_t& err)
+  {
+    std::cerr << err.whatFile() << ':' << err.whatLine() << ": " << err.what() << std::endl;
+    std::cerr << "=== STACK TRACE ===" << std::endl;
+    std::cerr << err.whatTrace() << std::endl;
+    return -1;
+  }
+
+  return 0;
+}
